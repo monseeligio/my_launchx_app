fafeb38b2220ecd6e42dbecc22d0c346b90a5a34
monseeligio <monserrateligio@gmail.com>
2022-04-22 21:31:26 -0500

    3. Creacion de un clase y modularizacion

---
+++ index.js
@@ -0,0 +1,7 @@
+const MissionCommander = require('./app/missionCommander');
+const carlo = new MissionCommander("Carlo")
+console.log(carlo.name)
+const monse = new MissionCommander("Monse")
+console.log(monse.name)
+const Jose = new MissionCommander("Jose")
+console.log(Jose.name)
